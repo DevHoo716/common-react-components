7af2b579a4cffb5470d09413499d289dc03e0cab
DH <79854016+DevHoo716@users.noreply.github.com>
2023-05-10 13:20:15 +0800

    remove unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
-import UnoCSS from "unocss/vite";
 import react from "@vitejs/plugin-react-swc";
 import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [UnoCSS(), react(), dts()],
+  plugins: [react(), dts()],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
